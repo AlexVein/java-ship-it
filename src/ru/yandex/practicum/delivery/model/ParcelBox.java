6694b1976c0662198be5f0e7c65b595bf1ccb6c2
Aleksei Velichkin <mail@avelichkin.ru>
2025-10-21 22:02:43 +0300

    feat(PercelBox): Add a field for the remaining free weight in the box.

--- src/ru/yandex/practicum/delivery/model/ParcelBox.java
+++ src/ru/yandex/practicum/delivery/model/ParcelBox.java
@@ -5,18 +5,20 @@ import java.util.ArrayList;
 public class ParcelBox<T extends Parcel> {
     private final ArrayList<T> packedParcels = new ArrayList<>();
 
-    private int weight;
+    private final int weight;
+    private int freeWeight;
 
     public ParcelBox(int weight) {
         this.weight = weight;
+        this.freeWeight = weight;
     }
 
     public int getFreeWeight() {
-        return weight;
+        return freeWeight;
     }
 
     public void addParcel(T parcel) {
-        if (weight - parcel.getWeight() < 0) {
+        if (freeWeight - parcel.getWeight() < 0) {
             System.err.printf("""
                             Невозможно добавить посылку %s в коробку.
                             Вес посылки составляет %d кг. Коробка может вместить еще %d кг.
@@ -26,7 +28,7 @@ public class ParcelBox<T extends Parcel> {
         }
 
         packedParcels.add(parcel);
-        weight -= parcel.getWeight();
+        freeWeight -= parcel.getWeight();
     }
 
     public ArrayList<T> getAllParcels() {
